d9154386915b865ff1a136db710717dd1ae729d1
Leandro <olafsteven1@gmail.com>
2019-05-08 14:24:44 +0200

    added different functionalities

--- firstDay/code/app/src/main/java/ch/ny/firstday/MainActivity.java
+++ firstDay/code/app/src/main/java/ch/ny/firstday/MainActivity.java
@@ -2,6 +2,8 @@ package ch.ny.firstday;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickBackButton(View v){
+        System.out.println("clicked Back");
+    }
+
+    public void onClickLoginButton(View v){
+        Toast.makeText(v.getContext(), "Login successful", Toast.LENGTH_LONG).show();
     }
 }
