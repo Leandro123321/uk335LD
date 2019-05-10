649fd1d3b6d5f9673a385b60a46e8255d59483e9
Leandro <olafsteven1@gmail.com>
2019-05-10 16:47:20 +0200

    search

--- ModulProjekt/code/app/src/main/java/ch/ny/searchactivity/SearchActivity.java
+++ ModulProjekt/code/app/src/main/java/ch/ny/searchactivity/SearchActivity.java
@@ -5,8 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.TextView;
 
+import java.io.Console;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,17 +22,20 @@ public class SearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        //Testdaten erstellen
-        List<ListViewObject> itemList = new ArrayList<>();
-        for (int i = 1; i <= 30; i++) {
-            ListViewObject item1 = new ListViewObject();
-            item1.setItemName("Random Item");
-            item1.setItemNumber(i);
-            itemList.add(item1);
-        }
+        SearchView searchView = findViewById(R.id.searchcity);
 
-        ListView listView = findViewById(R.id.listCities);
-        listView.setAdapter(new ListViewAdapter(this, itemList));
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                UpdateListView(newText);
+                return false;
+            }
+        });
     }
 
     public void onClickOpenDetails(View sendButton) {
@@ -39,5 +44,19 @@ public class SearchActivity extends AppCompatActivity {
         Intent showNameActivityIntent = new Intent(this, DetailsActivity.class);
         showNameActivityIntent.putExtra("key_city_name", name);
         startActivity(showNameActivityIntent);
+    }
+
+    public void UpdateListView(String text){
+        //Testdaten erstellen
+        List<ListViewObject> itemList = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            ListViewObject item1 = new ListViewObject();
+            item1.setItemName(text);
+            item1.setItemNumber(i);
+            itemList.add(item1);
+        }
+
+        ListView listView = findViewById(R.id.listCities);
+        listView.setAdapter(new ListViewAdapter(this, itemList));
     }
 }
