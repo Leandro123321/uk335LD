fb96f63bea140396ecb4341a84976f49b63cb3ed
Leandro <olafsteven1@gmail.com>
2019-05-10 10:52:33 +0200

    Added navigation

--- ModulProjekt/code/app/src/main/java/ch/ny/detailsactivity/DetailsActivity.java
+++ ModulProjekt/code/app/src/main/java/ch/ny/detailsactivity/DetailsActivity.java
@@ -1,7 +1,9 @@
 package ch.ny.detailsactivity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import ch.ny.homeactivity.R;
 
@@ -11,5 +13,11 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+
+        Intent intent = getIntent();
+        String City = intent.getStringExtra("key_city_name");
+        TextView cityLabel = findViewById(R.id.textView);
+        cityLabel.setText(City);
     }
 }
