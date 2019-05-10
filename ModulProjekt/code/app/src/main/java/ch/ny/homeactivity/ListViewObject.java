55119f72dff25a00f0f6a7d522eded157e21df11
Bastian Steingruber <steingruberbastian@gmail.com>
2019-05-10 08:57:49 +0200

    Created ruf design of the search activity

---
+++ ModulProjekt/code/app/src/main/java/ch/ny/homeactivity/ListViewObject.java
@@ -0,0 +1,23 @@
+package ch.ny.homeactivity;
+
+public class ListViewObject {
+    private String itemName;
+
+    private int itemNumber;
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getItemNumber() {
+        return itemNumber;
+    }
+
+    public void setItemNumber(int itemNumber) {
+        this.itemNumber = itemNumber;
+    }
+}
