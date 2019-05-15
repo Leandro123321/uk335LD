0df2172634320080ab7a1c85bf662297513991e1
Duarte Ramos <dudanos1205@gmail.com>
2019-05-15 13:53:22 +0200

    DB Version 2

--- ModulProjekt/code/app/src/main/java/ch/ny/connections/AppDatabase.java
+++ ModulProjekt/code/app/src/main/java/ch/ny/connections/AppDatabase.java
@@ -8,7 +8,7 @@ import android.content.Context;
 import ch.ny.Entity.City;
 import ch.ny.dao.CityDao;
 
-@Database(entities = City.class, version = 1)
+@Database(entities = City.class, version = 2)
 public abstract class AppDatabase extends RoomDatabase {
     private static final String DB_NAME = "db_weather_db";
     private static AppDatabase appDb;
